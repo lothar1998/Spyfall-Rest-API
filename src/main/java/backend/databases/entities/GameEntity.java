360a0668ce2ffa7fe2b276da209ba32dab8f5824
kamkali <kamkalis@gmail.com>
2019-08-29 20:46:44 +0200

    Upgrading Entities - adding relations by reference and lazy loading

--- src/main/java/backend/databases/entities/GameEntity.java
+++ src/main/java/backend/databases/entities/GameEntity.java
@@ -3,9 +3,18 @@ package backend.databases.entities;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
-import java.time.LocalDateTime;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.GregorianCalendar;
+
+/**
+ * Game entity
+ * @author Kamil Kaliś
+ */
 
 @Document(collection = "games")
 @NoArgsConstructor
@@ -14,8 +23,24 @@ public class GameEntity {
 
     @Id
     private String id;
+
+    @Field(value = "host")
+    @NotNull
+    @NotBlank
+    @DBRef(lazy = true)
     private UserEntity hostId;
-    private LocalDateTime gameStart;
+
+    @Field(value = "gameTime")
+    @NotNull
+    private GregorianCalendar gameStart;
+
+    @Field(value = "gameLocation")
+    @NotNull
+    @DBRef(lazy = true)
     private LocationEntity location;
-    private RoleEntity players;
+
+    @Field(value = "palyerRole")
+    @NotNull
+    @DBRef(lazy = true)
+    private RoleEntity roles;
 }
--- src/main/java/backend/databases/entities/RoleEntity.java
+++ src/main/java/backend/databases/entities/RoleEntity.java
@@ -4,6 +4,15 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Role entity
+ * @author Kamil Kaliś
+ */
 
 @Document(collection = "roles")
 @NoArgsConstructor
@@ -12,6 +21,14 @@ public class RoleEntity {
 
     @Id
     private String id;
+
+    @Field(value = "roleName")
+    @NotNull
+    @NotBlank
     private String name;
+
+    @Field(value = "description")
+    @NotNull
+    @NotBlank
     private String description;
 }
